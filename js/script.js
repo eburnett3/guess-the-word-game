da6cd6221e10d46ec872d576287f8372b41d1971
Eric <eburnett32@gmail.com>
2023-04-18 15:01:00 -0500

    added some more functions that added guesses to an array

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
     const placeholderLetters = [];
@@ -24,5 +25,38 @@ button.addEventListener("click", function (e) {
     e.preventDefault();
     const guess = letterInput.value;
     console.log(guess);
+    message.innerText = "";
+
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+    
+        makeGuess(guess);
+    }
     letterInput.value = "";
-})
+});
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]]/;
+    if (input.length === 0) {
+        message.innerText = "Please, enter a letter";
+    } else if (input.lenth > 1) {
+        message.innerText = "Only enter one letter at a time";
+    }   else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You have already guessed that letter. Try again.";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+}
+
+console.log(goodGuess);
