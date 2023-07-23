894385476fd5fb7003324fb390abc315b4c4c706
Eric <eburnett32@gmail.com>
2023-07-23 12:25:21 -0500

    added an async function

--- js/script.js
+++ js/script.js
@@ -11,6 +11,19 @@ let word = "magnolia";
 const guessedLetters = [];
 let remainingGuesses = 8;
 
+const getWord = async function () {
+  const response = await fetch(
+    "https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt"
+  );
+  const words = await response.text();
+  const wordArray = words.split("\n");
+  const randomIndex = Math.floor(Math.random() * wordArray.length);
+  word = wordArray[randomIndex].trim();
+  placeholder(word);
+};
+
+getWord();
+
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
